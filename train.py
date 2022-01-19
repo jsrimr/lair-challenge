e2b24544f2d280cf4cdf971db28b162341c13bc8
jsrimr <jsrimr@naver.com>
2022-01-19 13:40:26 +0900

    save_top_k, earlystop callback 추가

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 # from pytorch_lightning.loggers import NeptuneLogger
 from pytorch_lightning.loggers import WandbLogger
 from torch import nn
@@ -48,16 +48,31 @@ def main(config):
 
     wandb_logger = WandbLogger(project="lair-challenge")
     wandb_logger.watch(model.cnn)
-    wandb_logger.watch(model.rnn)
+    # wandb_logger.watch(model.rnn)
     
     # model.load_state_dict(torch.load(save_path, map_location=device))
 
-    checkpoint_callback = ModelCheckpoint(monitor="score",
-                                          save_top_k=3, mode="max")
 
+    checkpoint_callback = ModelCheckpoint(
+        monitor='score',
+        # dirpath='.checkpoint',
+        filename='{epoch}-{val_score:.3f}',
+        save_top_k=3,
+        mode='max',
+        save_weights_only=True,
+    )
+
+    early_stop_callback = EarlyStopping(
+                    monitor='val_loss',
+                    patience=3,
+                    verbose=False,
+                    mode='min'
+                    )
     # limit train_batches=20
     trainer = Trainer(max_epochs=epochs, gpus=2, precision=16,
-                      default_root_dir=os.getcwd(), callbacks=[checkpoint_callback], logger=wandb_logger, 
+                      default_root_dir=os.getcwd(), 
+                      log_every_n_steps=5,
+                      callbacks=[checkpoint_callback], logger=wandb_logger, 
                       strategy='ddp')
 
     # trainer.tune(model)
